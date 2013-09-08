9f27c95883e39ed69f60fae8f33db70f0f25731e
liuji <liujiboy@163.com>
2013-09-08 20:00:21 +0800

    add 001

--- 001/knn.py
+++ 001/knn.py
@@ -3,29 +3,27 @@ def loadData(fileName):
     file=open(fileName)
     firstLine=file.readline()
     cols=firstLine.strip().split(",")
-    data={}
+    data=[]
     for line in file.readlines():
         values=line.strip().split(",");
         name=values[0];
-        data[name]={"name":name,"type":values[-1],"attrs":array([float(i) for i in values[1:-1]])}
+        data.append({"name":name,"type":values[-1],"attrs":array([float(i) for i in values[1:-1]])})
     return cols,data
 def dist(a,b):
     return sqrt(sum((a-b)**2))
 def find(item,data,k):
     distances=[]
-    for key in data:
-        value=data[key]
+    for value in data:
         d=dist(item["attrs"],value["attrs"])
-        distances.append((key,d))
-    distances.sort(key=lambda x:x[1],reverse=True)
-    distances=distances[0:k]
+        distances.append(d)
+    indices=argsort(distances)
+    indices=indices[0:k]
     typesCount={}
-    print distances
-    for i in distances:
-        type=data[i[0]]["type"]
+    for i in indices:
+        type=data[i]["type"]
         typesCount[type]=typesCount.get(type,0)+1
-    types=[(type,count) for type,count in typesCount.items()]
-    types.sort(key=lambda x:x[1],reverse=True)
+    types=[(count,type) for type,count in typesCount.items()]
+    types.sort(reverse=True)
     print types
     
 if __name__=="__main__":
